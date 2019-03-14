7ab32e2c8031bd635f0e081209284d3314715434
Lam Ngo <ngohoanglam65@gmail.com>
2019-03-14 09:37:58 +0700

    Add JDBCConnection class, create connection to mySQL db

--- src/dao/impl/JDBCConnection.java
+++ src/dao/impl/JDBCConnection.java
@@ -1,5 +1,23 @@
 package dao.impl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+//create JDBC connection
 public class JDBCConnection {
 
+	public Connection getConn() {
+		String username = "root";
+		String password = "";
+		String url = "jdbc:mysql://localhost:3306/web_ban_sach";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(url, username, password);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
