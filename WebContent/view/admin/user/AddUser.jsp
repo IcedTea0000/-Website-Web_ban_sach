609e4b0978c0de9af62383096055cd25baa3d7e3
Lam Ngo <ngohoanglam65@gmail.com>
2019-03-14 14:19:41 +0700

    Add 'add user' controller and view

--- WebContent/view/admin/user/AddUser.jsp
+++ WebContent/view/admin/user/AddUser.jsp
@@ -1,12 +1,44 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Add New User</title>
 </head>
 <body>
-
+	<div>
+		<div>
+			<h1>Add new user</h1>
+		</div>
+		<div>
+			<form method="post" action="add-user">
+				<div>
+					NAME <input type="text" name="name" value="${param.name}" required="required" autofocus="autofocus" />
+				</div>
+				<div>
+					ADDRESS <input type="text" value="${param.address}" name="address">
+				</div>
+				<div>
+					USERNAME <input type="text" name="username" value="${param.username}" required="required" />
+					${checkUserExisted}
+				</div>
+				<div>
+					PASSWORD <input type="text" name="password" required="required" />
+				</div>
+				<div>
+					ROLE <br /> ADMIN <input type="radio" name="role" value="admin">
+					MEMBER <input type="radio" name="role" value="member"
+						checked="checked">
+				</div>
+				<div>
+					ABOUT YOU <br />
+					<textarea rows="5" cols="20" name="info" placeholder="Tell us something about you" >${param.info}</textarea>
+				</div>
+				<hr />
+				<button type="submit">Submit Information</button>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
---
+++ src/controller/user/AddUserController.java
@@ -0,0 +1,51 @@
+package controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+import service.UserService;
+import service.impl.UserServiceImpl;
+
+@WebServlet(urlPatterns = { "/admin/add-user" })
+public class AddUserController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/view/admin/user/AddUser.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		String address = req.getParameter("address");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		String role = req.getParameter("role");
+		String info = req.getParameter("info");
+		UserServiceImpl userService = new UserServiceImpl();
+
+		if (!userService.checkUserNameExisted(username)) {
+			User newUser = new User(name, address, username, password, role, info);
+			// add newUser to SQL db
+			userService.add(newUser);
+			// forward to search "all" result
+
+		}
+		else {
+			req.setAttribute("checkUserExisted", "Username existed");
+			RequestDispatcher dispatcher = req.getRequestDispatcher("/view/admin/user/AddUser.jsp");
+			dispatcher.forward(req, resp);
+		}
+			
+		
+	}
+
+}
