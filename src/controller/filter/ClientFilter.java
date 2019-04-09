afd5d8eaa089e221d296d6d06f8ddfac0b895dac
Lam Ngo <ngohoanglam65@gmail.com>
2019-04-09 12:07:54 +0700

    Add filters

--- src/controller/filter/ClientFilter.java
+++ src/controller/filter/ClientFilter.java
@@ -1,5 +1,49 @@
 package controller.filter;
 
-public class ClientFilter {
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter (urlPatterns={"/user-account","/cart/checkout","/order-history","/user-update"})
+public class ClientFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request=(HttpServletRequest)req;
+		HttpSession session=request.getSession();
+		
+		//check if client login
+		if (session.getAttribute("userAccount")!=null){
+			chain.doFilter(req, resp);
+		}
+		else
+		{
+			HttpServletResponse response=(HttpServletResponse)resp;
+			response.sendRedirect("/GreatBookList/login");
+		}
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
---
+++ src/controller/filter/NoAccessFilter.java
@@ -0,0 +1,38 @@
+package controller.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter (urlPatterns={"/download"})
+public class NoAccessFilter implements Filter  {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse resp=(HttpServletResponse)arg1;
+		resp.sendRedirect("/GreatBookList/home");
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
